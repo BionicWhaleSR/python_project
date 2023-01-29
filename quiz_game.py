77f67264e5f89f9eaf46e9257d54ca78324c90a2
Saul Rosales <samuel.rosales777@outlook.com>
2023-01-29 12:26:18 -0500

    First question and response

---
+++ quiz_game.py
@@ -0,0 +1,16 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+	quit()
+
+print("Okay! Let's play :)")
+
+anwer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+	print('Correct!')
+else:
+	print("Incorrect!")
+
+quit()
